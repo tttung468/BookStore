3db7e9ef747fd3c8c9e46ff1adc6c778fdc94342
ThanhTung468 <tttung468@users.noreply.github.com>
2022-05-10 00:16:36 +0700

    Add file gitignore

--- src/Business/BookStore.Business/RepositoryInterfaces/IBookRepository.cs
+++ src/Business/BookStore.Business/RepositoryInterfaces/IBookRepository.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Threading.Tasks;
-using BookStore.Business.Entities;
+﻿using BookStore.Business.Entities;
 
 namespace BookStore.Business.RepositoryInterfaces
 {
